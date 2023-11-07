060eca7f3a83cf0121458318de4a562985cd4359
Athulya Ganesh <athulya.ganesh2001@gmail.com>
2023-11-07 18:00:24 -0500

    global variables

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+
+using namespace std; 
+
+// Define the number of philosophers and bottles
+#define NUM_PHILOSOPHERS 5
+#define NUM_BOTTLES 10 
+#define SUBSET_BOTTLES 2 
+
+int main() 
+{
+   
+}
